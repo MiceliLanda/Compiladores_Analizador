a252d18c18e7f47124ef39a8d3217ec38db3bd42
MiceliLanda <183381@ids.upchiapas.edu.mx>
2022-02-03 18:33:52 -0600

    first-tests

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,37 @@
+import re
 
-a = '       new-db hola (escuela)'
-b = a.strip()
+def funcion(metodo, resto):
+    resto.pop(0)
+    if metodo == 'new-db':
+        createDB(resto)
+    elif metodo == 'take':
+        takeDB(resto)
+    elif metodo == 'new-struct':
+        pass
+    elif metodo == 'fix-struct':
+        pass
 
-if b.find('new-db') == 0:
-    print(b)
-else: print('debe comenzar con palabra rese')
+def createDB(sql):
+    arg = ''.join(sql)
+    if arg.isalnum():
+        if len(sql) > 0  and len(sql) < 2:
+            print('[OK] : DB Created Successfully')
+        print('[ERROR] : Check your MIBA Syntax')
+    print('[ERROR] : Check your MIBA Syntax')
+
+def takeDB(sql):
+    arg = ''.join(sql)
+    if arg.isalnum():
+        if len(sql) > 0  and len(sql) < 2:
+            print(f'[OK] : DB Used {arg}')
+        print('[ERROR] : Check your MIBA Syntax')
+    print('[ERROR] : Check your MIBA Syntax')
+
+reservadas = [ 'new-db', 'new-struct' , 'take' , 'fix-struct']
+entrada = input() # ENTRADA DE TEXTO
+b = entrada.lower().strip().split(' ') #PARTIMOS Y QUITAMOS ESPACIOS DE LA SENTENCIA
+# if b.__getitem__(0).isalpha(): #VERIFICAMOS SI CONTIENE CARACTERES ESPECIALES
+if b.__getitem__(0) in reservadas: # VERIFICAMOS SI COINCIDE CON ALGUNA PALABRA RESERVADA
+    funcion(b.__getitem__(0),b) # LLAMOS LA FUNCIÓN CORRESPONDIENTE
+else: print(f'[ERROR] : Syntax Error :  {b.__getitem__(0)}')
+# else: print(f'[ERROR] : Syntax Error :  {b.__getitem__(0)}')
