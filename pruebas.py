0ccbb3a41d7d55d1b4de6203b0717e7fa6a356e9
MiceliLanda <183381@ids.upchiapas.edu.mx>
2022-03-03 19:42:17 -0600

    new-struct Ok

--- pruebas.py
+++ pruebas.py
@@ -1,104 +1,148 @@
-# import re
+tiposDato = ['int','varchar', 'bool', 'double']
 
-# def outputMessageError():
-#     print('[ERROR] : Check your MIBA Syntax')
+def advanceSetence(sentencia):
+    pila = ['$',')','T','R','L','(','R','L']
+    entrada = list(sentencia)
+    entrada.reverse()
+    if entrada.pop() in 'new-struct':
+        print('Reserved : 1')
+    else: 
+        print('ERROR : No se encuentra palabra reservada')
 
-# def outputMessageSuccess(num, msg):
-#     if num == 0:
-#         print('[OK] : DB Created Successfully')
-#     elif num == 1:
-#         print(f'[OK] : DB Used {msg}')
-#     elif num == 2:
-#         print(f'[OK] : Struct {msg} DELETED')
-#     elif num == 3:
-#         print(f'[OK] : DB {msg} DELETED')
+    print('ENTRADA ADVANCE ', entrada)
+    #
+    nombre = list(entrada.pop())
+    nombre.reverse()
+    print('\nNombre de la tabla : ',''.join(reversed(nombre)))
+    print('Pila gramática : ',pila,'\n')
+    for i in reversed(nombre):
+        if not i.isalnum():
+            print('Caracter inválido crack  mira crack-> ',i)
+            new = ''.join(reversed(nombre))
+            entrada.append(new)
+        else:
+            nombre.pop(),pila.pop()
+            print('Actual : ',' '.join(reversed(nombre)), '  - Verificando : ',i)
+            print(pila)
+            if pila.pop() == 'R':
+                pila.extend(list('RL'))
+                if not nombre:
+                    pila.clear(),pila.extend(list('$)TRL('))
+                    print('Pila gramática : ',pila,'\n')
+                else: print('R - > L')
+    # pila.pop(),pila.pop()
+    # print('LO QUE SE VA AL firstParent -> ',entrada , pila)
+    firstParent(entrada, pila)  
 
-# def funcion(metodo, resto):
-#     resto.pop(0)
-#     if metodo == 'new-db':
-#         createDB(resto)
-#     elif metodo == 'take':
-#         takeDB(resto)
-#     elif metodo == 'new-struct':
-#         pass
-#     elif metodo == 'fix-struct':
-#         pass
-#     elif metodo == 'supr-struct':
-#         suprStruct(resto)
-#     elif metodo == 'supr-db':
-#         suprDB(resto)
+def firstParent(entrada,pila):
+    if  len(entrada[-1]) > 1 and pila[-1] == entrada[-1][0]:
+        clean = entrada[-1][1::]
+        entrada.pop()
+        pila.pop()
+        entrada.append(clean)
+        print('if 1 : ',entrada)
+        checkName(entrada, pila)
+    elif '(' == entrada[-1]:
+        pila.pop(),entrada.pop()
+        print('if 2: ',entrada)
+        checkName(entrada, pila)
+    else: 
+        print('ERROR: Se fue alv todo, naah mentira crack , solo no pusiste parentesis')
 
-# def createDB(sql):
-#     arg = ''.join(sql)
-#     if arg.isalnum():
-#         if len(sql) > 0  and len(sql) < 2:
-#             outputMessageSuccess(0,arg)
-#         else: outputMessageError()
-#     else: outputMessageError()
+def endParentesis(entrada,pila):
+    pila.clear()
+    print('final',entrada[-1][-1])
 
-# def takeDB(sql):
-#     arg = ''.join(sql)
-#     if arg.isalnum():
-#         if len(sql) > 0  and len(sql) < 2:
-#             outputMessageSuccess(1,arg)
-#         else: outputMessageError()
-#     else: outputMessageError()
+    if entrada[-1] == ')':
+        print('parentesisCierre suma \n Ejecución Ok')
+        entrada.pop()
+        pila.append('$'),entrada.append('$')
+        print(f'Entrada end : {entrada} Pila end : {pila}')
+    elif entrada[-1][-1] == ')':
+        entrada.pop()
+        entrada.append('$')
+        pila.append('$')
+        print('parentesisCierre suma \n Ejecución Ok')
+        print(f'Entrada end : {entrada} Pila end2 : {pila}')
+    else: print('ERROR : Se espera un parenteis de cierre')
 
-# def suprStruct(sql):
-#     arg = ''.join(sql)
-#     if arg.isalnum():
-#         if len(sql) > 0  and len(sql) < 2:
-#             outputMessageSuccess(2,arg)
-#         else: outputMessageError()
-#     else: outputMessageError()
+def checkName(entrada, pila): # atri int, algo bool)
+    sentence = entrada
+    name = list(sentence.pop())
+    print('Entrada : ',''.join(name))
+    name.reverse()
+    print(pila)
+    for i in reversed(name):
+        # if not i in L or i in D:
+        if not i.isalnum():
+            print('Error : Caracter no válido (',i,')')
+            new = ''.join(reversed(name))
+            sentence.append(new)
+            verify = False
+            break
+        else:
+            name.pop()
+            pila.pop()
+            print('Entrada Actual :',''.join(reversed(name)), '  - Verificando : ',i)
+            print(pila)
+            if pila.pop() == 'R':
+                pila.extend(list('RL'))
+                print(pila)
+        verify = True
+    if verify:
+        # tokens['identificadores'] += 1
+        print('Indentificador 1')
+        pila.pop(),pila.pop()
+        print('\nEntrada Name eliminado : ',sentence,' Pila Name: ',pila)
+        # return entrada, pila
+        checkDato(entrada, pila)
+    else: print('Indentificador 0')
+    # else: tokens['identificadores'] = 0
+def checkSeparador(entrada,pila):
+    """ CHECAR QUE AUMENTE EL CONTADOR DE SEPARADOR EN EL DE TOKENS """
 
-# def suprDB(sql):
-#     arg = ''.join(sql)
-#     if arg.isalnum():
-#         if len(sql) > 0  and len(sql) < 2:
-#             outputMessageSuccess(3,arg)
-#         else: outputMessageError()
-#     else: outputMessageError()
+    print('Entrada chekSperador : ',entrada[-1], ' Pila check separador : ',pila)
+    if len(entrada[-1]) > 1 and entrada[-1][-1] == ',': #CUANDO LLEGA CON TIPO DE DATO
+        clean = entrada[-1][0:len(entrada[-1])-1]
+        entrada.pop(),entrada.append(clean)
+        print('SALI DE SEPARADOR PARA ENTRAR A CHECKDATO 1-> ',entrada)
+        checkDato(entrada, pila)
+    elif entrada[-1] == ',': #ESTE CASO ES CUANDO LLEGA LA COMA SOLA EJ. ','
+        entrada.pop()
+        print('SALI DE SEPARADOR PARA ENTRAR A CHECKDATO 2-> ',entrada)
+        checkDato(entrada, pila)
 
-# reservadas = [ 'new-db', 'new-struct' , 'take' , 'fix-struct', 'supr-db', 'supr-struct']
-# entrada = input() # ENTRADA DE TEXTO
-# b = entrada.lower().strip().split(' ') #PARTIMOS Y QUITAMOS ESPACIOS DE LA SENTENCIA
-# # if b.__getitem__(0).isalpha(): #VERIFICAMOS SI CONTIENE CARACTERES ESPECIALES
-# if b.__getitem__(0) in reservadas: # VERIFICAMOS SI COINCIDE CON ALGUNA PALABRA RESERVADA
-#     funcion(b.__getitem__(0),b) # LLAMOS LA FUNCIÓN CORRESPONDIENTE
-# else: print(f'[ERROR] : Syntax Error :  {b.__getitem__(0)}')
-# # else: print(f'[ERROR] : Syntax Error :  {b.__getitem__(0)}')
+def checkDato(entrada, pila): #int, algo bool)5 
+    # print('entrada checkDato : ',entrada ,' checkdatoPila : ',pila)
+    print('Antes del cualquier if de checkdato : ', entrada[-1],'\n')
+    if ',' in entrada[-1]:
+        print('Tiene coma pegado al tipo : ',entrada[-1])
+        checkSeparador(entrada, pila)
+    elif ')' in entrada[-1] and entrada[-1][0:len(entrada[-1])-1] in tiposDato:
+        print('Tiene parentesis, es único : ',entrada[-1])
+        endParentesis(entrada, pila)
+    else:
+        print('No tiene coma ni parentesis de cierre, se checa si es un dato válido : ',entrada[-1])
+        # print('Size entrada: ',len(entrada))
+        if len(entrada) > 1 and len(entrada) <4: # entrada = '$' ')' 'int'
+            # print('Size entrada dentro del if de size: ',len(entrada))
+            if entrada[-1] in tiposDato:
+                print('Se encontró un tipo parecido = ',entrada[-1])
+                entrada.pop(),pila.pop(),pila.extend(list('TRL'))
+                print('size :',len(entrada), entrada)
+                checkName(entrada,pila)
+            else:
+                print('No se encontró un tipo, eliminado y Termina')
+        else:
+            print('QUE ES ENTRADA ',entrada[-1])
+            if entrada[-1] in tiposDato:
+                entrada.pop(),pila.pop(),pila.extend(list('TRL'))
+                print('Entra al else porque no es el ultimo : ',entrada,'\nPIla: ',pila)
+                checkName(entrada,pila)
+            else: 
+                print('Tipo no valido: ' ,entrada[-1])
 
+#""" ME QUEDÉ EN VERIFICAR SI EL ULTIMO ELEMENTO DE LA ENTRADA ES SOLO COMA IF 139 """
 
-# s = 'edad2','edad=5','edad=5'
-# a = len(s)
-# cont = 0
-# for elemento in s:
-#     if cont != a:
-#         if '=' in elemento:
-#             cont+=1
-#             print('hacer separacion por signo igual') #proceso de evaluar ->funcion
-#         else: 
-#             print(f'Check your variable: {elemento}')
-#             break
+advanceSetence('new-struct Alumnos (atri1 int, atri2 bool, atri3 varchar, asd double)'.strip(' ').lower().split(' '))
 
-# value = 'b=2,c=0,b=,2'
-
-# co = value.count(',')
-# ig = value.count('=')
-
-# if value.count('=')-1 == value.count(','):
-#     print('OK')
-# else:
-#     print('error syntax')
-metodo = 'new-db'
-token = {'new-db' : 1,'take-db' : 0,'supr-db' : 1,'supr-struct' : 3}
-
-token.update({'new-db' : token.get(metodo)+1})
-# print('\nantes de borrar' ,token,'\n')
-
-# for i,clave in enumerate(tokens):
-#     if tokens.get(clave) > 0:
-#         tokens[clave] = 0
-for i,v in enumerate(token):
-    print(f'{v} : {i}')
